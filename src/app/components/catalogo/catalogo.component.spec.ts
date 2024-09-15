c621c588d8331019a46eb1c8cbca077819f59526
Miguel2003Q <miguelmorales.10923@gmail.com>
2024-09-15 17:30:20 -0500

    creando el servicio CartServicesService

--- src/app/components/catalogo/catalogo.component.spec.ts
+++ src/app/components/catalogo/catalogo.component.spec.ts
@@ -1,23 +1,113 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
 
-import { CatalogoComponent } from './catalogo.component';
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrl: './catalogo.component.scss'
+})
 
-describe('CatalogoComponent', () => {
-  let component: CatalogoComponent;
-  let fixture: ComponentFixture<CatalogoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [CatalogoComponent]
-    })
-    .compileComponents();
+export class CatalogoComponent {
 
-    fixture = TestBed.createComponent(CatalogoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  
+  // crea un método para añadir un producto al carrito
+  addToCart(product: any) { 
+    console.log(product)
+  }
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  // actulizar el array de productos con el atributo cantidad en cada producto
+  products = [
+    {
+      id:1,
+      name: 'Bike_1',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
+    {
+      id:2,
+      name: 'Bike_2',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
+    {
+      id:3,
+      name: 'Bike_3',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    {
+      id:4,
+      name: 'Bike_4',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
+    {
+      id:5,
+      name: 'Bike_5',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    {
+      id:6,
+      name: 'Bike_6',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
+    {
+      id:7,
+      name: 'Bike_7',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    {
+      id:8,
+      name: 'Bike_8',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    },
+    {
+      id:9,
+      name: 'Bike_9',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    {
+      id:19,
+      name: 'Bike_10',
+      price: 120,
+      description: 'Product Description',
+      inventory: 10,
+      image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      cantidad: 0
+    }, 
+    
+]
+
+}
---
+++ src/app/test-name.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+@Injectable({
+  providedIn: 'root'
+})
+export class TestNameService {
+  private testName = 'Hola Mundo';
+
+  constructor() { }
+
+  getTestName(): string {
+    return this.testName;
+  }
+}
